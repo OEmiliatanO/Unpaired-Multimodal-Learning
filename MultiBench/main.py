05805cdcaa1e70b3e39371eae897d73f0b8f0cd0
OEmiliatanO <Emiliaistruelove@gmail.com>
2026-01-04 16:18:08 +0800

    change the naming rule; the key of results now uses wandb convention to separate train, val and test stage

--- MultiBench/main.py
+++ MultiBench/main.py
@@ -35,65 +35,68 @@ parser.add_argument('--debug', action='store_true')
 parser.add_argument('--run_name', type=str, default='')
 parser.add_argument('--results_dir', type=str, default='./results')
 parser.add_argument('--log_dir', type=str, default='./logs')
+parser.add_argument('--alpha_y', type=float, default=1.0)
 
 
-
+# change this to your data directory
+data_dir = './'
 def main(args):
-    log_dir = args.log_dir
-    fname = f"log_{args.run_name}{args.ds_name}_mod{args.modality}_zdim{args.zdim}_epochs{args.num_epochs}_pos_embd_{args.pos_embd}_learnable_{args.pos_learnable}_step_k{args.step_k}_n_seeds{args.n_seeds}_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
-    results_dir = os.path.join(args.results_dir, fname)
-    
-    if not args.debug:  
-        os.makedirs(log_dir, exist_ok=True)
-        os.environ["WANDB_DIR"] = log_dir + "/wandb"
-        wandb.init(entity="<USENAME>", project="<PROJECTNAME>", name = fname)
-        wandb.config.update(args)
-
     print("Command-line arguments:", sys.argv)
     print("Parsed arguments:", args)
 
+    exp_name = f"log_{args.run_name}{args.ds_name}/mod{args.modality}/epochs{args.num_epochs}/zdim{args.zdim}/alpha_y{args.alpha_y}/step_k{args.step_k}/pos_embd_{args.pos_embd}_learnable_{args.pos_learnable}/lr{args.lr}"
+    results_dir = os.path.join(args.results_dir, exp_name)
     seeds = [i for i in range(args.n_seeds)]
-    outs = {'score_x': [], 'score_y': [], 'score_xy': [], 'val_score_x': [], 'val_score_y': [], 'val_score_xy': []}
+    outs = {'test/score_x': [], 'test/score_y': [], 'test/score_xy': [], 'val/score_x': [], 'val/score_y': [], 'val/score_xy': []}
     for seed in seeds:
         set_seed(seed)
+        log_dir = args.log_dir
+        seed_dir = os.path.join(results_dir, f"seed_{seed}")
+        print(f"Results will be saved to {seed_dir}")
+        if not args.debug:  
+            os.makedirs(log_dir, exist_ok=True)
+            os.environ["WANDB_DIR"] = log_dir + "/wandb"
+            wandb.init(entity="unpaired_multimodal", project="unpaired_multimodal", tags=[args.ds_name, args.modality, "self-supervised"], name = os.path.join(exp_name, f"seed_{seed}"), reinit="finish_previous")
+            wandb.config.update(args)
+            wandb.config.update({'seed': seed})
         if args.ds_name == 'mosi':
             batch_size=32
             indims = [20, 300]
             # Sample two train loaders to shuffle x-only and y-only data within each dataloader
-            train_loader, *_ = get_dataloader('./data_files/mosi_data.pkl', robust_test=False, batch_size=batch_size, train_shuffle=True)
-            train_loader_2, *_ = get_dataloader('./data_files/mosi_data.pkl', robust_test=False, batch_size=batch_size, train_shuffle=True)
-            eval_train_loader, eval_valid_loader, eval_test_loader = get_dataloader('./data_files/mosi_data.pkl', robust_test=False, batch_size=batch_size, train_shuffle=False)
+            train_loader, *_ = get_dataloader(f'{data_dir}/data_files/mosi_data.pkl', robust_test=False, batch_size=batch_size, train_shuffle=True)
+            train_loader_2, *_ = get_dataloader(f'{data_dir}/data_files/mosi_data.pkl', robust_test=False, batch_size=batch_size, train_shuffle=True)
+            eval_train_loader, eval_valid_loader, eval_test_loader = get_dataloader(f'{data_dir}/data_files/mosi_data.pkl', robust_test=False, batch_size=batch_size, train_shuffle=False)
 
         elif args.ds_name == 'sarcasm':
             batch_size=128
             indims = [371, 300]
-            train_loader, *_ = get_dataloader('./data_files/sarcasm.pkl', batch_size=batch_size, data_type='sarcasm', vision_norm=True)
-            train_loader_2, *_ = get_dataloader('./data_files/sarcasm.pkl', batch_size=batch_size, data_type='sarcasm', vision_norm=True)
-            eval_train_loader, eval_valid_loader, eval_test_loader = get_dataloader('./data_files/sarcasm.pkl', batch_size=batch_size, data_type='sarcasm', train_shuffle=False, vision_norm=True)
+            train_loader, *_ = get_dataloader(f'{data_dir}/data_files/sarcasm.pkl', batch_size=batch_size, data_type='sarcasm', vision_norm=True)
+            train_loader_2, *_ = get_dataloader(f'{data_dir}/data_files/sarcasm.pkl', batch_size=batch_size, data_type='sarcasm', vision_norm=True)
+            eval_train_loader, eval_valid_loader, eval_test_loader = get_dataloader(f'{data_dir}/data_files/sarcasm.pkl', batch_size=batch_size, data_type='sarcasm', train_shuffle=False, vision_norm=True)
             
 
         elif args.ds_name == 'humor':
             batch_size=128
             indims = [371, 300]
-            train_loader, *_ = get_dataloader('./data_files/humor.pkl', batch_size=batch_size, data_type='humor')
-            train_loader_2, *_ = get_dataloader('./data_files/humor.pkl', batch_size=batch_size, data_type='humor')
-            eval_train_loader, eval_valid_loader, eval_test_loader = get_dataloader('./data_files/humor.pkl', batch_size=batch_size, data_type='humor', train_shuffle=False)
+            train_loader, *_ = get_dataloader(f'{data_dir}/data_files/humor.pkl', batch_size=batch_size, data_type='humor')
+            train_loader_2, *_ = get_dataloader(f'{data_dir}/data_files/humor.pkl', batch_size=batch_size, data_type='humor')
+            eval_train_loader, eval_valid_loader, eval_test_loader = get_dataloader(f'{data_dir}/data_files/humor.pkl', batch_size=batch_size, data_type='humor', train_shuffle=False)
             
         elif args.ds_name == 'mimic':
             from datasets.mimic.get_data import get_dataloader as get_mimic_dataloader
             batch_size = 128
             indims = [5, 12]
-            train_loader, *_ = get_mimic_dataloader(7, batch_size=batch_size, imputed_path='./data_files/im.pk')
-            train_loader_2, *_ = get_mimic_dataloader(7, batch_size=batch_size, imputed_path='./data_files/im.pk')
-            eval_train_loader, eval_valid_loader, eval_test_loader = get_mimic_dataloader(7, imputed_path='./data_files/im.pk', train_shuffle=False)        
+            train_loader, *_ = get_mimic_dataloader(7, batch_size=batch_size, imputed_path=f'{data_dir}/data_files/im.pk')
+            train_loader_2, *_ = get_mimic_dataloader(7, batch_size=batch_size, imputed_path=f'{data_dir}/data_files/im.pk')
+            eval_train_loader, eval_valid_loader, eval_test_loader = get_mimic_dataloader(7, imputed_path=f'{data_dir}/data_files/im.pk', train_shuffle=False)        
             eval_test_loader = eval_valid_loader # as per FACTOR-CL codebase
         
         elif args.ds_name == 'mosei':
             batch_size=32
             indims = [35, 300]
-            train_loader, *_ = get_dataloader('./data_files/mosei_senti_data.pkl', robust_test=False, batch_size=batch_size, data_type='mosei', train_shuffle=True)
-            train_loader_2, *_ = get_dataloader('./data_files/mosei_senti_data.pkl', robust_test=False, batch_size=batch_size, data_type='mosei', train_shuffle=True)
-            eval_train_loader, eval_valid_loader, eval_test_loader = get_dataloader('./data_files/mosei_senti_data.pkl', robust_test=False, batch_size=batch_size, data_type='mosei', train_shuffle=False)
+            train_loader, *_ = get_dataloader(f'{data_dir}/data_files/mosei_senti_data.pkl', robust_test=False, batch_size=batch_size, data_type='mosei', train_shuffle=True)
+            train_loader_2, *_ = get_dataloader(f'{data_dir}/data_files/mosei_senti_data.pkl', robust_test=False, batch_size=batch_size, data_type='mosei', train_shuffle=True)
+            eval_train_loader, eval_valid_loader, eval_test_loader = get_dataloader(f'{data_dir}/data_files/mosei_senti_data.pkl', robust_test=False, batch_size=batch_size, data_type='mosei', train_shuffle=False)
         else:
             raise NotImplementedError('Dataset not implemented yet')
 
@@ -115,19 +118,34 @@ def main(args):
         optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
         # Train model
-        score = train(model, args.modality,train_loader, train_loader_2,optimizer, 
-                        num_epoch=args.num_epochs, step_k=args.step_k,ds_name=args.ds_name,
-                        eval_config={'train': eval_train_loader, 'val': eval_valid_loader, 'test': eval_test_loader, 'freq': 100},
-                        augment=args.augment, debug=args.debug)
+        capture_embeddings_during_training = True
+        results = train(model, args.modality, train_loader, train_loader_2, optimizer, 
+                    num_epoch=args.num_epochs, step_k=args.step_k, ds_name=args.ds_name,
+                    eval_config={'train': eval_train_loader, 'val': eval_valid_loader, 'test': eval_test_loader, 'freq': 100},
+                    alpha_x=1.0, alpha_y=args.alpha_y, capture_embeddings_during_training=capture_embeddings_during_training,
+                    augment=args.augment, debug=args.debug)
+        
+        if capture_embeddings_during_training:
+            score, embeddings = results
+        else:
+            score = results
 
         print('seed: ', seed, ' score: ', score)
         print('=====================================')
-        outs['score_x'].append(100*score[0])
-        outs['score_y'].append(100*score[1])
-        outs['score_xy'].append(100*score[2])
-        outs['val_score_x'].append(100*score[3])
-        outs['val_score_y'].append(100*score[4])
-        outs['val_score_xy'].append(100*score[5])
+        outs['test/score_x'].append(100*score['test/score_x'])
+        outs['test/score_y'].append(100*score['test/score_y'])
+        outs['test/score_xy'].append(100*score['test/score_xy'])
+        outs['val/score_x'].append(100*score['val/score_x'])
+        outs['val/score_y'].append(100*score['val/score_y'])
+        outs['val/score_xy'].append(100*score['val/score_xy'])
+
+        # save model and log outputs
+        os.makedirs(seed_dir, exist_ok=True)
+        print(f"Saving model to {os.path.join(seed_dir, 'model.pth')}")
+        torch.save(model.state_dict(), os.path.join(seed_dir, "model.pth"))
+        torch.save(score, os.path.join(seed_dir, "results.pth"))
+        if capture_embeddings_during_training:
+            torch.save(embeddings, os.path.join(seed_dir, "embeddings.pth"))
 
     print(outs)
     
@@ -136,24 +154,16 @@ def main(args):
     outs_std = {k: np.std(v) for k, v in outs.items()}
     print("Final scores (mean): ", outs_mean)
     print("Final scores (std): ", outs_std)
-    
-    if not args.debug:
-        wandb.log({f'final_{k}': v for k, v in outs_mean.items()})
-        wandb.log({f'final_std_{k}': v for k, v in outs_std.items()})
 
     # save model and log outputs
     os.makedirs(results_dir, exist_ok=True)
-    print(f"Saving model to {os.path.join(results_dir, 'model.pth')}")
-    torch.save(model.state_dict(), os.path.join(results_dir, "model.pth"))
-    with open(os.path.join(results_dir, "outputs.txt"), "w") as f:
-        f.write(f"Final scores: {outs_mean}\n")
-        f.write(f"Final scores std: {outs_std}\n")
-
+    torch.save(outs, os.path.join(results_dir, "results.pth"))
 
 if __name__ == "__main__":
     outer_parser = argparse.ArgumentParser(description="MultiBench Experiment")
     outer_parser.add_argument("-c", "--config", type=str, default="config.json", help="Configuration file")
     outer_parser.add_argument("-s", "--slurm", action="store_true", help="Launched with slurm")
+    outer_parser.add_argument("-r", "--run", action="store_true", help="Run the experiments")
     outer_parser.add_argument("-d", "--outer_debug", action="store_true", help="Debug mode")
     outer_parser.add_argument("-f", "--flag", action="store_true", help="Run despite existing experiments directory")
     outer_parser.add_argument("-o", "--overwrite", action="store_true", help="Overwrite existing experiments directory")
@@ -178,6 +188,10 @@ if __name__ == "__main__":
     print("Total combinations:", len(combinations))
     for i, combo in enumerate(combinations):
         print(f"Combination {i}: {combo}")
+    # modification: only run if --run is specified
+    if not outer_args.run:
+        print("Exiting without running experiments (use -r to run).")
+        sys.exit(0)
     if outer_args.slurm:
         # args.debug = True
         job_id = int(os.getenv("SLURM_ARRAY_TASK_ID", "-1"))
@@ -190,4 +204,10 @@ if __name__ == "__main__":
         args.overwrite = outer_args.overwrite
         print("=> Parsed arguments:", args)
         main(args)
-
+    else:
+        for i, combination in enumerate(combinations):
+            print(f"=> Running combination {i}: {combination}")
+            args = parser.parse_args([], argparse.Namespace(**combination))
+            args.overwrite = outer_args.overwrite
+            print("=> Parsed arguments:", args)
+            main(args)
