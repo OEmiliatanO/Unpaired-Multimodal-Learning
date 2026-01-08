2297cefe61620dd43d0a4e1e3612ed676d3a1aff
OEmiliatanO <Emiliaistruelove@gmail.com>
2026-01-08 12:00:43 +0800

    correct the data generation code

--- Gaussian_experiment/data.py
+++ Gaussian_experiment/data.py
@@ -7,17 +7,17 @@ from utils import make_reproducible
 #            'dim_obs': 50, 'noise_std': 0.1, 'attenuate_x': True, 'attenuation': 0.05}
 def generate_data(configs):
     make_reproducible(configs['seed'])
-    theta_c = torch.randn(configs['num_samples'], configs['dim_c'])
-    theta_x = torch.randn(configs['num_samples'], configs['dim_x'])
-    theta_y = torch.randn(configs['num_samples'], configs['dim_y'])
+    theta_c = torch.randn(configs['dim_c'])
+    theta_x = torch.randn(configs['dim_x'])
+    theta_y = torch.randn(configs['dim_y'])
 
     noise_x = torch.randn(configs['num_samples'], configs['dim_obs']) * configs['noise_std']
     noise_y = torch.randn(configs['num_samples'], configs['dim_obs']) * configs['noise_std']
 
-    A_c = torch.randn(configs['dim_obs'], configs['dim_c'])
-    A_x = torch.randn(configs['dim_obs'], configs['dim_x'])
-    B_c = torch.randn(configs['dim_obs'], configs['dim_c'])
-    B_y = torch.randn(configs['dim_obs'], configs['dim_y'])
+    A_c = torch.randn(configs['num_samples'], configs['dim_obs'], configs['dim_c'])
+    A_x = torch.randn(configs['num_samples'], configs['dim_obs'], configs['dim_x'])
+    B_c = torch.randn(configs['num_samples'], configs['dim_obs'], configs['dim_c'])
+    B_y = torch.randn(configs['num_samples'], configs['dim_obs'], configs['dim_y'])
 
     if configs['attenuate_x']:
         attenuation = torch.full((configs['dim_c'],), configs['attenuation'])
@@ -27,10 +27,12 @@ def generate_data(configs):
         theta_c_x = theta_c
         
     # X = Ac * (W * theta_c) + Ax * theta_x + eps_X
-    data_x = (theta_c_x @ A_c.T) + (theta_x @ A_x.T) + noise_x
+    data_x = torch.einsum('ndc,c->nd', A_c, theta_c_x) + torch.einsum('ndx,x->nd', A_x, theta_x) + noise_x
+    # data_x = (theta_c_x @ A_c.T) + (theta_x @ A_x.T) + noise_x
 
     # Y = Bc * theta_c + By * theta_y + eps_Y
-    data_y = (theta_c @ B_c.T) + (theta_y @ B_y.T) + noise_y
+    # data_y = (theta_c @ B_c.T) + (theta_y @ B_y.T) + noise_y
+    data_y = torch.einsum('ndc,c->nd', B_c, theta_c) + torch.einsum('ndy,y->nd', B_y, theta_y) + noise_y
 
     return {'x': data_x, 'y': data_y}
 
